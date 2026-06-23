f7e04ca8051d975f8eacf661c811b52a9218b5dd
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-06-23 17:02:34 +0530

    Modifying Object State Referenced by a Final Variable

--- Gen.java
+++ Gen.java
@@ -1,11 +1,20 @@
-public class Gen{
-    public static void main(String args[]){
-       final int[] arr = {10, 20, 30};
+// public class Gen{
+//     public static void main(String args[]){
+//        final int[] arr = {10, 20, 30};
        
-       arr[2] = 99;
+//        arr[2] = 99;
 
-       for(int i = 0; i < arr.length; i++){
-        System.out.println(arr[i] + " ");
-       }
+//        for(int i = 0; i < arr.length; i++){
+//         System.out.println(arr[i] + " ");
+//        }
+//     }
+// }
+
+public class Gen{
+    int p = 20;
+    public static void main(String args[]){
+        final Gen t = new Gen();
+        t.p = 30;
+        System.out.println(t.p);
     }
 }
