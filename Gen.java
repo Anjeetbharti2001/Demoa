a4bfd9a2d26e19d6e446ec24a86dbb774ce51fb1
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-06-24 11:26:33 +0530

    Find the node and Edges

--- Gen.java
+++ Gen.java
@@ -1,24 +1,33 @@
 class Node {
     int data;
-    Node left;
-    Node right;
-
+    Node left, right;
     Node(int data){
         this.data = data;
-        this.left = null;
-        this.right = null;
+        left = null;
+        right = null;
     }
 }
 public class Gen{
-    public static void main(String args[]){
+    // Total nodes count
+    static int countNodes(Node root){
+        if(root == null){
+            return 0;
+        }
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+    public static void main(String [] args){
+        //tree creation
         Node root = new Node(1);
-
         root.left = new Node(2);
-        root.right = new Node (3);
 
-        root.left = new Node(4);
-        root.right = new Node(5);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
 
-        System.out.println("root : " + root.data);
+        int nodes = countNodes(root);
+        int edges = nodes - 1;
+
+        System.out.println("Total Nodes = " + nodes);
+        System.out.println("Total Edges = " + edges);
     }
 }
