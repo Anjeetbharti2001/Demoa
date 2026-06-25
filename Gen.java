261ad53dda20ea4516b0189d9aea910305d1c0f8
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-06-25 11:55:57 +0530

    Merge Two arrays Using Heap

--- Gen.java
+++ Gen.java
@@ -1,13 +1,14 @@
 import java.util.*;
 public class Gen{
     public static void main(String args[]){
-        int [] arr = {4, 2, 7, 1, 3};
+        int[] a = {1, 4, 7};
+        int [] b = {2, 3, 6};
+
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int num : arr){
-            pq.add(num);
-        }
+        for(int num : a) pq.add(num);
+        for(int num : b) pq.add(num);
         while(!pq.isEmpty()){
-            System.out.println(pq.poll() + " ");
+            System.out.print(pq.poll() + " ");
         }
     }
 }
