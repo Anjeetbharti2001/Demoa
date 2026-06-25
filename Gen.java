b615732d821b01caf78206d486ea96daa51825f6
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-06-25 11:39:14 +0530

    Check if Heap is Empty

--- Gen.java
+++ Gen.java
@@ -1,25 +1,17 @@
+import java.util.*;
 public class Gen {
     public static void main(String args[]){
-        int nums[] = new int[7];
-        nums[0] = 0;
-        nums[1] = 1;
-        nums[2] = 1;
-        nums[3] = 1;
-        nums [4] = 2;
-        nums[5] = 2;
-        nums[6] = 3;
-        System.out.println(mergeSortedArray(nums));
-    }
-    public static int mergeSortedArray(int[]nums){
-        int i = 0;
-        for(int j = 1; j < nums.length; j++){
-           if(nums[i] < nums[i]){
-            int temp = nums[i + 1];
-            nums[i + 1] = nums[j];
-            nums[j] = temp;
-            i++;
-           }
+        PriorityQueue<Integer>pq = new PriorityQueue<>();
+        pq.add(10);
+        pq.add(20);
+
+        while(!pq.isEmpty()){
+            pq.poll();
         }
-        return i + 1;
+        if(pq.isEmpty()){
+            System.out.println("heap is Empty");
+        }else{
+            System.out.println("Heap is not Empty");
+        }
     }
 }
