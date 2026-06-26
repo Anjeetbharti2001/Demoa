98c462b019230dd729ca554cb1aa9b998acfb5d5
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-06-26 10:33:05 +0530

    print the root node

--- Gen.java
+++ Gen.java
@@ -1,22 +1,40 @@
+// class Node {
+//     int data;
+//     Node left, right;
+//     Node(int data){
+//         this.data = data;
+//         left = null;
+//         right = null;
+//     }
+// }
+// public class Gen {
+//     public static void main(String args[]){
+//         Node root = new Node(1);
+
+//         root.left = new Node(2);
+//         root.right = new Node(3);
+
+//         root.left.left = new Node(4);
+//         root.left.right = new Node(5
 class Node {
     int data;
-    Node left, right;
+    Node left,right;
     Node(int data){
-        this.data = data;
-        left = null;
-        right = null;
+       this.data = data;
+       left = null;
+       right = null;
     }
 }
 public class Gen {
     public static void main(String args[]){
-        Node root = new Node(1);
+       Node root = new Node(5);
 
-        root.left = new Node(2);
-        root.right = new Node(3);
+       root.left = new Node(6);
+       root.right = new Node(7);
 
-        root.left.left = new Node(4);
-        root.left.right = new Node(5);
+       root.left.left = new Node(8);
+       root.left.right = new Node(8);
 
-        System.out.println("root:-" + root.data);
+       System.out.println("root:- " + root.data);
     }
 }
