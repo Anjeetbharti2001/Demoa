26680391731cdfaf7d47064c33e48eca6f9bed68
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-06-26 10:40:44 +0530

    Import the arrayList Class

--- Gen.java
+++ Gen.java
@@ -1,40 +1,12 @@
-// class Node {
-//     int data;
-//     Node left, right;
-//     Node(int data){
-//         this.data = data;
-//         left = null;
-//         right = null;
-//     }
-// }
-// public class Gen {
-//     public static void main(String args[]){
-//         Node root = new Node(1);
+import java.util.*;
 
-//         root.left = new Node(2);
-//         root.right = new Node(3);
-
-//         root.left.left = new Node(4);
-//         root.left.right = new Node(5
-class Node {
-    int data;
-    Node left,right;
-    Node(int data){
-       this.data = data;
-       left = null;
-       right = null;
-    }
-}
 public class Gen {
     public static void main(String args[]){
-       Node root = new Node(5);
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("Anjeet");
+        names.add("bharti");
+        names.add("Sarjun");
 
-       root.left = new Node(6);
-       root.right = new Node(7);
-
-       root.left.left = new Node(8);
-       root.left.right = new Node(8);
-
-       System.out.println("root:- " + root.data);
+        System.out.println(names);
     }
 }
