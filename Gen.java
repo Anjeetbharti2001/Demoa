7750e7748e978dbecbd667892a7606feebc1dc86
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-06-27 10:45:06 +0530

    print the node root

--- Gen.java
+++ Gen.java
@@ -1,12 +1,18 @@
-import java.util.*;
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+
+    }
+}
 
 public class Gen {
     public static void main(String args[]){
-        ArrayList<String> names = new ArrayList<String>();
-        names.add("Anjeet");
-        names.add("bharti");
-        names.add("Sarjun");
-
-        System.out.println(names);
+        Node root = new Node(10);
+        System.out.println("Root:-" + root.data);
     }
 }
