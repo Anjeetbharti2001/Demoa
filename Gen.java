83b45f9f300f7e18e2919bc610a99d7aa57308d6
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-06-27 10:54:02 +0530

    Tree Created Successfully

--- Gen.java
+++ Gen.java
@@ -12,7 +12,13 @@ class Node {
 
 public class Gen {
     public static void main(String args[]){
-        Node root = new Node(10);
-        System.out.println("Root:-" + root.data);
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.right = new Node(4);
+        root.left.right = new Node(5);
+        System.out.println("Tree created successfull");
+        // Node root = new Node(10);
+        // System.out.println("Root:-" + root.data);
     }
 }
