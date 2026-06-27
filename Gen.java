59e05020d9febfbfe86fe600b617782bbcb44f91
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-06-27 11:25:26 +0530

    Count total nodes

--- Gen.java
+++ Gen.java
@@ -1,23 +1,24 @@
-class Node {
+class Node{
     int data;
     Node left, right;
-
-    Node(int data) {
+    Node(int data){
         this.data = data;
         left = null;
         right = null;
-
     }
 }
+public class Gen{
+   static int countNode(Node root) {
+    if (root == null) {
+        return 0;
+    }
 
-public class Gen {
+    return 1 + countNode(root.left) + countNode(root.right);
+}
     public static void main(String args[]){
-        Node root = new Node(1);
-        root.left = new Node(2);
-        root.right = new Node(3);
-        root.left.right = new Node(4);
-        root.left.right = new Node(5);
-        
-        System.out.println("Root.left.right data:-" + root.data);
+         Node root = new Node(4);
+         root.left = new Node(5);
+         root.right = new Node(6);
+         System.out.println("Total Nodes :-" + countNode(root));
     }
-}
+}    
