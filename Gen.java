e4993462df78cfe8e2e0017b487561be8a6644ce
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-06-27 11:55:04 +0530

    total multiplication

--- Gen.java
+++ Gen.java
@@ -1,28 +1,9 @@
-class Node{
-    int data;
-    Node left, right;
-    Node(int data){
-        this.data = data;
-        left = null;
-        right = null;
-    }
-}
-public class Gen{
-   static int countNode(Node root) {
-    if (root == null) {
-        return 0;
-    }
-
-    return 1 + countNode(root.left) + countNode(root.right);
-}
+public class Gen {
     public static void main(String args[]){
-         Node root = new Node(4);
-         root.left = new Node(5);
-         root.right = new Node(6);
-         root.left.left = new Node(7);
-         root.right.left = new Node(8);
-         root.right.right = new Node(9);
-         
-         System.out.println("Total Nodes :-" + countNode(root));
+        int a = 5;
+        int b = 6;
+
+        int sum = a*b;
+        System.out.println("Total multi:-" + sum );
     }
-}    
+}
