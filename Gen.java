71889df20645c8d411b8679500f4ae1ccd46bbd7
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-06-28 10:50:39 +0530

    Height of the tree

--- Gen.java
+++ Gen.java
@@ -8,7 +8,12 @@ class Node{
     }
 }
 public class Gen {
-    public static void main(String args[]){
+    static int height(Node root) {
+    if (root == null) {
+        return 0;
+    }
+    return 1 + Math.max(height(root.left), height(root.right));
+}    public static void main(String args[]){
         Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
@@ -31,6 +36,6 @@ public class Gen {
         root.right.right.left = new Node(14);
         root.right.right.right = new Node(15);
 
-        System.out.println("Print the root value :-" + root.data);
+        System.out.println("Height of Tree :-" + height(root));
     }
 }
