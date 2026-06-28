c951c055e0fd4df83d3e2bc274a75eb696570206
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-06-29 04:08:32 +0530

    Selection Sort

--- Gen.java
+++ Gen.java
@@ -1,22 +1,27 @@
 public class Gen {
-    static void bubbleSort(int arr[]){
+    static void selectionSort(int arr[]){
         int n = arr.length;
+        
+        for(int i = 0; i < n - 1; i++){
+            int min = i;
 
-        for(int i = 0; i<n-1; i++){
-            for(int j = 0; j < n - i - 1; j++){
-                if(arr[j] > arr[j + 1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+            for(int j = i + 1; j < n; j ++){
+                if(arr[j] < arr[min]){
+                    if(arr[j] < arr[min]){
+                        min = j;
+                    }
                 }
+                int temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = temp;
             }
         }
     }
     public static void main(String args[]){
-        int arr[] = {5, 1, 4, 2, 8};
-        bubbleSort(arr);
+        int arr[] = {64, 25, 12, 22, 11};
+        selectionSort(arr);
         for(int num : arr)
-            System.out.print(num + " ");
+            System.out.println(num + " ");
 
     }
 }
