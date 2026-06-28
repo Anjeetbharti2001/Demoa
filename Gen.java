bb12aa1157d9ac251514b0ba7526b3aee184367d
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-06-29 05:00:29 +0530

    Quick Sort

--- Gen.java
+++ Gen.java
@@ -1,49 +1,39 @@
+// Quick Sort 
 public class Gen {
-    static void merge(int arr[], int left, int mid, int right){
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
-        
-        int L[] = new int[n1];
-        int R[] = new int[n2];
+    static int partition(int arr[], int low, int high){
+        int pivot = arr[high];
+        int i = low - 1; 
 
-        for(int i = 0; i < n1; i++)
-            L[i] = arr[left + i];
-        
-        for(int j = 0; j < n2; j++)
-            R[j] = arr[mid + 1 + j];
+        for(int j = low; j < high; j++){
+            if(arr[j] < pivot){
+                i++;
 
-        int i = 0, j = 0, k = left;
-
-        while(i < n1 && j < n2){
-            if(L[i] <= R[j])
-                arr[k++] = L[i++];
-            else
-                arr[k++] = R[j++];
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
         }
-        while(i < n1)
-            arr[k++] = L[i++];
-        while(j < n2)
-            arr[k++] = R[j++];
+        int temp = arr[i + 1];
+        arr[i + 1 ] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
     }
-    static void mergeSort(int arr[], int left, int right){
-        if(left < right){
 
-            int mid = (left + right) / 2;
+    static void quickSort(int arr[], int low, int high){
+        if(low < high){
+            int pi = partition(arr, low, high);
 
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid + 1, right);
-
-            merge(arr, left, mid, right);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
         }
     }
     public static void main(String args[]){
-        int arr[] = {38, 27, 43, 3, 9, 82, 10};
+        int arr[] = {10, 7, 8, 9, 1, 5};
 
-        mergeSort(arr, 0, arr.length - 1);
-
+        quickSort(arr, 0, arr.length - 1);
         for(int num : arr){
-            System.out.print( num + " ");
+            System.out.print(num + " ");
         }
-        
     }
 }
