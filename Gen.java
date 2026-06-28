1750ce742ab30fd7194f47efa4ab5cfa7c9b895a
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-06-29 04:17:20 +0530

    Insertion Sort

--- Gen.java
+++ Gen.java
@@ -1,27 +1,24 @@
+// Insertion Sort
 public class Gen {
-    static void selectionSort(int arr[]){
+    static void insertionSort(int arr[]){
         int n = arr.length;
-        
-        for(int i = 0; i < n - 1; i++){
-            int min = i;
+        for(int i = 1; i < n ; i++){
+            int key = arr[i];
+            int j = i - 1;
 
-            for(int j = i + 1; j < n; j ++){
-                if(arr[j] < arr[min]){
-                    if(arr[j] < arr[min]){
-                        min = j;
-                    }
-                }
-                int temp = arr[min];
-                arr[min] = arr[i];
-                arr[i] = temp;
+            while(j >= 0 && arr[j] > key){
+                arr[j + 1] = arr[j];
+                j--;
             }
+            arr[j + 1] = key;
         }
     }
     public static void main(String args[]){
-        int arr[] = {64, 25, 12, 22, 11};
-        selectionSort(arr);
+        int arr[] = {12, 11, 13, 5, 6};
+        insertionSort(arr);
+
         for(int num : arr)
-            System.out.println(num + " ");
+            System.out.print(num + " ");
 
     }
 }
