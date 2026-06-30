98ce6073e7dbcaf1b5d9d78c820b8a35bcf356d5
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-06-30 19:46:25 +0530

    calling and defining multiple fuction

--- Gen.java
+++ Gen.java
@@ -1,9 +1,17 @@
-public class Gen {
+public class Gen{
     public static void main(String args[]){
-        message();
-        System.out.println("CRY, and you stop the monotony!");
+        System.out.println("I am main");
+        italy();
+        brazil();
+        argentina();
     }
-    static void message(){
-        System.out.println("Smile the world smailes with you..");
+    static void italy(){
+        System.out.println("I am in italy");
+    }
+    static void brazil(){
+        System.out.println("I am in brazil");
+    }
+    static void argentina(){
+        System.out.println(" I am in argentina");
     }
 }
