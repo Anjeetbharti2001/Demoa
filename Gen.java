c660434d53f1381a5d3fc05a8730785bc007f72d
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-06-30 20:17:39 +0530

    Communication Between Function

--- Gen.java
+++ Gen.java
@@ -1,17 +1,20 @@
+import java.util.*;
 public class Gen{
     public static void main(String args[]){
-        System.out.println("I am main");
-        italy();
-        brazil();
-        argentina();
+       
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any Three Numbers:-");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int sum = sum1(a, b, c);
+        System.out.println("sum :-" + sum);
+        sc.close();
     }
-    static void italy(){
-        System.out.println("I am in italy");
+    static int sum1(int x, int y, int z){
+         int d = x + y + z;
+         return d;
     }
-    static void brazil(){
-        System.out.println("I am in brazil");
-    }
-    static void argentina(){
-        System.out.println(" I am in argentina");
-    }
+   
 }
