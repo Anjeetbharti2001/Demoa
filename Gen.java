a9e9042454169c8011ddc1ff5f42226501d66a9e
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-01 08:19:10 +0530

    count Total Node

--- Gen.java
+++ Gen.java
@@ -8,10 +8,16 @@ class Node{
     }
 }
 public class Gen {
+    static int countNode(Node root){
+        if(root == null){
+            return 0;
+        }
+        return 1 + countNode(root.left) + countNode(root.right);
+    }
     public static void main(String args[]){
          Node root = new Node(1);
          root.left = new Node(2);
          root.right = new Node(3);
-         System.out.println("Print Node:-" + root.data);
+         System.out.println("Print total Node:-" + countNode(root));
     }
 }
