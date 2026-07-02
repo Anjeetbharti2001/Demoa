5ca9c79f1afcedd413dea3e3b7656c32a59ebab5
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-03 04:25:00 +0530

    print the lowest age in the arrays

--- Gen.java
+++ Gen.java
@@ -1,15 +1,16 @@
-// create a program that calculates the average of different ages
 public class Gen {
     public static void main(String args[]){
-        int ages[] = {20, 22, 18, 35, 48, 26, 87, 70};
+        int ages[] = {20, 22, 18, 35, 48, 26, 27, 70};
 
-        float avg, sum = 0;
-        // Get the length of the arrays 
         int length = ages.length;
+
+        int lowestAge = ages[0];
+
         for(int age : ages){
-            sum += age;
+            if(lowestAge > age){
+                lowestAge = age;
+            }
         }
-        avg = sum / length;
-        System.out.println("the average age is :-" + avg);
+        System.out.println("The lowest age in the arrays is :" + lowestAge);
     }
 }
