ba9848829a2d1e9246a7dc5d4f870721438789c5
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-03 03:49:39 +0530

    create an arrays of strings and then used a for loops to print each element

--- Gen.java
+++ Gen.java
@@ -1,30 +1,8 @@
-class Node {
-    int data;
-    Node left, right;
-    Node(int data){
-        this.data = data;
-        left = null;
-        right = null;
-    }
-}
-public class Gen{
-     static int height(Node root){
-            if(root == null){
-                return 0;
-            }
-            return 1 + Math.max(height(root.left), height(root.right));
-        }
+public class Gen {
     public static void main(String args[]){
-       Node root = new Node(4);
-
-       root.left = new Node(5);
-       root.right = new Node(6);
-
-       root.left.left = new Node(7);
-       root.left.right = new Node(8);
-
-       root.right.left = new Node(9);
-       root.right.right = new Node(10);
-       System.out.println("Total Height of Tree:-" + height(root));
+        String[] names = {"Anjeet ", "Sarjun", "Sohan","Pawan"};
+        for(int i = 0; i< names.length; i++){
+            System.out.println(names[i]);
+        } 
     }
 }
