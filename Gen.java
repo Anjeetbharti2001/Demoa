f7466943cd2dc85ed0b82c9653946dca5a1025a4
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-05 01:17:16 +0530

    change Element values

--- Gen.java
+++ Gen.java
@@ -1,6 +1,8 @@
 public class Gen {
     public static void main(String args[]){
         int [][] myNumbers = {{1, 4, 2}, {3, 6, 8}};
-        System.out.println(myNumbers[0][1]); // Outputs 8
+        myNumbers[1][2] = 9;
+        System.out.println(myNumbers[1][2]);
+       
     }
 }
