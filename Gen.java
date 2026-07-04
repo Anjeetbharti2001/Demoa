390c2caf72ff874fd673122409e443092dabf06d
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-05 01:14:06 +0530

    this example print the values at row 0, column

--- Gen.java
+++ Gen.java
@@ -1,6 +1,6 @@
 public class Gen {
     public static void main(String args[]){
         int [][] myNumbers = {{1, 4, 2}, {3, 6, 8}};
-        System.out.println(myNumbers[1][2]); // Outputs 8
+        System.out.println(myNumbers[0][1]); // Outputs 8
     }
 }
