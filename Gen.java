0bbb6f6baf30e5244f64f0d6ffea33374591876b
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-07 17:19:56 +0530

    Inorder an Arrays Traversal

--- Gen.java
+++ Gen.java
@@ -1,32 +1,54 @@
-public class Gen {
-    static int binarySearch(int arr[], int target){
-        int left = 0; 
-        int right = arr.length - 1;
-        while(left <= right){
-            int mid = left + (right - left)/2;
-            if(arr[mid] == target){
-                return mid;
-            }
-            else if(arr[mid] < target){
-                left = mid + 1;
-            }else if(arr[mid] < target){
-                left = mid + 1;
-            }else{
-                right = mid -1;
-            }
-        }
-        return - 1;
-    }
-    public static void main(String args[]){
-        int arr[] = {10, 20, 30, 40, 60, 70};
-        int target = 50;
+class Node {
+    int data;
+    Node left, right;
 
-        int index = binarySearch(arr, target);
-        if(index != -1){
-            System.out.println("Element found at index : " + index);
-        }else{
-            System.out.println("Element not found");
+    Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinaryTree {
+    static int idx = -1;
+
+    public Node buildTree(int[] nodes) {
+        idx++;
+
+        if (nodes[idx] == -1) {
+            return null;
         }
-        
+
+        Node newNode = new Node(nodes[idx]);
+        newNode.left = buildTree(nodes);
+        newNode.right = buildTree(nodes);
+
+        return newNode;
+    }
+}
+
+public class Gen {
+
+    // Inorder Traversal
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+
+        BinaryTree tree = new BinaryTree();
+
+        Node root = tree.buildTree(nodes);
+
+        System.out.print("Inorder Traversal: ");
+        inorder(root);
     }
 }
