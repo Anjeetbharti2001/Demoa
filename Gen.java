ed83a4fcdfeb3dc6d6eed10eabb3cda698034f98
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-09 13:19:17 +0530

    Multiple Parameter

--- Gen.java
+++ Gen.java
@@ -1,10 +1,10 @@
 public class Gen {
-    static void myMethod(String fname){
-        System.out.println(fname + "Refsnes");
+    static void myMethod(String fname, int age){
+        System.out.println(fname + "is" + age);
     }
     public static void main(String args[]){
-        myMethod("Anjeet");
-        myMethod("Arjit");
-        myMethod("Aman");
+        myMethod("lucky", 8);
+        myMethod("Murthiya",  5);
+        myMethod("Kailash", 31);
     }
 }
