22a99d8e79ebccdb30a39e0c3e84a30ee7a0099e
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-09 13:39:36 +0530

    You can also store the result in a variable

--- Gen.java
+++ Gen.java
@@ -1,9 +1,9 @@
-public class Gen{
+public class Gen {
     static int myMethod(int x, int y){
         return x + y;
     }
     public static void main(String args[]){
-        System.out.println(myMethod(5,3));
-
+        int z = myMethod(5, 3);
+        System.out.println(z);
     }
 }
