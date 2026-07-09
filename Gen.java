0907a5a7c06a8a1e85cd2f3453e89a6ddd16b965
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-09 13:26:04 +0530

    It is common to use if..else statements inside methods:

--- Gen.java
+++ Gen.java
@@ -1,10 +1,12 @@
 public class Gen {
-    static void myMethod(String fname, int age){
-        System.out.println(fname + "is" + age);
+    static void checkAge(int age ){
+        if(age > 18){
+            System.out.println("Access denied - you are not old enough! ");
+        }else {
+            System.out.println("Access granted - You are not Enough!");
+        }
     }
     public static void main(String args[]){
-        myMethod("lucky", 8);
-        myMethod("Murthiya",  5);
-        myMethod("Kailash", 31);
+        checkAge(20);
     }
 }
