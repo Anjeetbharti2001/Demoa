5f77d75e565007c6ec0d849b1fe5c47938b3d540
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-09 13:34:38 +0530

    Sum of a method's two parameters:

--- Gen.java
+++ Gen.java
@@ -1,9 +1,9 @@
-public class Gen {
-    static int myMethod(int x){
-        return 5 + x;
+public class Gen{
+    static int myMethod(int x, int y){
+        return x + y;
     }
-    
     public static void main(String args[]){
-        System.out.println(myMethod(3));
+        System.out.println(myMethod(5,3));
+
     }
 }
