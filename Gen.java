b8638aef8c37e9e65daefc2c4ef3a26217c45f12
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-09 13:29:50 +0530

    Return value

--- Gen.java
+++ Gen.java
@@ -1,12 +1,9 @@
 public class Gen {
-    static void checkAge(int age ){
-        if(age > 18){
-            System.out.println("Access denied - you are not old enough! ");
-        }else {
-            System.out.println("Access granted - You are not Enough!");
-        }
+    static int myMethod(int x){
+        return 5 + x;
     }
+    
     public static void main(String args[]){
-        checkAge(20);
+        System.out.println(myMethod(3));
     }
 }
