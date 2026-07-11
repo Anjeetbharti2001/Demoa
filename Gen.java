ee098ae6e2df122337d5f2bcde73dcb42d7bc4c1
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-11 17:19:58 +0530

    Find leaf

--- Gen.java
+++ Gen.java
@@ -1,10 +1,29 @@
-public class Gen {
-    static int doubleGame(int x){
-        return x * 2;
-    }
-    public static void main(String args[]){
-        for(int i = 1; i<= 5; i++){
-            System.out.println("Double of " + i +  " is" + doubleGame(i) ) ;
+class Node {
+        int data;
+        Node right, left;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
         }
+    }
+public class Gen {
+    static void printLeaf(Node root){
+        if(root == null){
+            return ;
+        }
+        if(root.left == null && root.right == null);
+        System.out.print(root.data + " ");
+
+        printLeaf(root.left);
+        printLeaf(root.right);
+    }
+    public static void main(String args[]) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        printLeaf(root);
     }
 }
