38b7aa67bb98f2c20366799a1b5454c170167c79
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-14 17:17:06 +0530

    Find an Element in an Arrays

--- Gen.java
+++ Gen.java
@@ -1,20 +1,16 @@
 public class Gen{
-    // Write a method for linear search
-    static int linearSearch(int[]arr, int key){
+    public static void main(String args[]){
+        int [] arr = {10, 20, 30, 40, 50};
+        int key = 30;
+
+        int index = -1;
+
         for(int i = 0; i<arr.length; i++){
-            if(arr[i] == key){
-                return i;
+            if(arr[i] == key ){
+                index = i;
+                break;
             }
         }
-        return -1;
-    }
-    public static void main(String args[]){
-        int [] arr = {5, 10, 15, 20, 25};
-        int result = linearSearch(arr, 20);
-        if(result != -1){
-            System.out.println("found at index:" + result);
-        }else{
-            System.out.println("not found");
-        }
+        System.out.println(index);
     }
 }
