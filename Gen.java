a502b75add46f50083ea6c08650ff2d0ba85c3cc
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-14 17:10:55 +0530

    write a method for linearSearch

--- Gen.java
+++ Gen.java
@@ -1,16 +1,20 @@
-public class Gen {
-    public static int removeDuplicates(int[] arr){
-        int i = 0;
-        for(int j = 1; j<arr.length; j++){
-            if(arr[i] != arr[j]){
-                i++;
-                arr[i] = arr[j];
+public class Gen{
+    // Write a method for linear search
+    static int linearSearch(int[]arr, int key){
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] == key){
+                return i;
             }
         }
-        return i + 1;
+        return -1;
     }
     public static void main(String args[]){
-        int [] arr = {1, 1, 2, 2, 3, 4, 4};
-        System.out.println(removeDuplicates(arr));
+        int [] arr = {5, 10, 15, 20, 25};
+        int result = linearSearch(arr, 20);
+        if(result != -1){
+            System.out.println("found at index:" + result);
+        }else{
+            System.out.println("not found");
+        }
     }
 }
