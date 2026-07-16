0bfa7d6349a0b9b8d5efcc3c164558d63f0011bf
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-16 17:08:26 +0530

    Check if Arrays Palindrome

--- Gen.java
+++ Gen.java
@@ -1,27 +1,20 @@
 public class Gen {
-
-    // Method to reverse the array
-    static void reverse(int[] arr) {
+    public static boolean isPalindrome(int[] arr) {
         int left = 0;
         int right = arr.length - 1;
 
         while (left < right) {
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-
+            if (arr[left] != arr[right]) {
+                return false;
+            }
             left++;
             right--;
         }
-
-        // Print reversed array
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
+        return true;
     }
 
-    public static void main(String[] args) {
-        int[] arr = {10, 30, 40, 60, 70, 90};
-        reverse(arr);
+    public static void main(String args[]) {
+        int[] arr = { 1, 2, 3, 2, 1 };
+        System.out.println(isPalindrome(arr));
     }
 }
