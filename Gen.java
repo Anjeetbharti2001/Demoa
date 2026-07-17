3dda8f9e468aa3a73e92513e3b91c2669aa8388e
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-17 17:11:26 +0530

    This statement creates a TreeSet object in Java that stores elements in sorted order automatically

--- Gen.java
+++ Gen.java
@@ -1,24 +1,13 @@
-public class Gen {
-    public static void moveZeros(int[] arr) {
-        int j = 0;
+import java.util.*;
+public class Gen{
+    public static void main(String args[]){
+        TreeSet<Integer> ts = new TreeSet<>();
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != 0) {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                j++;
-            }
-        }
+        ts.add(10);
+        ts.add(5);
+        ts.add(20);
+        ts.add(10); // duplicate (ignored)
 
-        // Print the array
-        for (int x : arr) {
-            System.out.print(x + " ");
-        }
-    }
-
-    public static void main(String args[]) {
-        int[] arr = {0, 1, 0, 3, 1, 2};
-        moveZeros(arr);
+        System.out.println(ts);
     }
 }
