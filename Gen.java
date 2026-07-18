46a47b9cbfe2b20b40a54de078cbb4bce64e7cfc
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-18 17:17:40 +0530

    Fix Window

--- Gen.java
+++ Gen.java
@@ -1,19 +1,20 @@
-import java.util.*;
-public class Gen{
-    public static void main(String args[]){
-        Set<String> ts = new TreeSet<>();
-
-        ts.add("bharat");
-        ts.add("india");
-        ts.add("Nepal");
-        ts.add("A");
-        ts.add("B");
-        ts.add("Z");
-
-        for(String value : ts){
-            System.out.println(value + " ,");
+public class Gen {
+    static int maxSum(int [] arr, int k){
+        int windowSum = 0;
+        for(int i = 0; i< k; i++){
+            windowSum += arr[i];
         }
-        System.out.println();
-
+        int maxSum = windowSum ;
+        for(int i = k; i<arr.length; i++){
+            windowSum = windowSum + arr[i] - arr[i - k];
+            maxSum = Math.max(maxSum, windowSum);
+        }
+        return maxSum;
     }
+    public static void main(String args[]){
+        int [] arr = {2, 1, 5, 1, 3 , 2};
+
+        System.out.println(maxSum(arr,3));
+    }
+    
 }
