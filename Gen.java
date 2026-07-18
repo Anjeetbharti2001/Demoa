ea1fa3e1d08d554613d3f579acc73eae8a99286d
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-18 17:28:28 +0530

    moves all Zeros to End

--- Gen.java
+++ Gen.java
@@ -1,20 +1,20 @@
-public class Gen {
-    static int maxSum(int [] arr, int k){
-        int windowSum = 0;
-        for(int i = 0; i< k; i++){
-            windowSum += arr[i];
+public class Gen{
+    public static void moveZeros(int [] arr){
+        int j = 0;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] != 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[i] = temp;
+                j++;
+            }
         }
-        int maxSum = windowSum ;
-        for(int i = k; i<arr.length; i++){
-            windowSum = windowSum + arr[i] - arr[i - k];
-            maxSum = Math.max(maxSum, windowSum);
+        for(int x : arr){
+         System.out.println(x + " ");
         }
-        return maxSum;
     }
     public static void main(String args[]){
-        int [] arr = {2, 1, 5, 1, 3 , 2};
-
-        System.out.println(maxSum(arr,3));
+        int [] arr = {0, 1, 0, 3, 12};
+        moveZeros(arr);
     }
-    
 }
