81340617647d7ceb8797dd419143ea1a994716cc
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-19 09:03:24 +0530

    Selection Sort

--- Gen.java
+++ Gen.java
@@ -1,15 +1,17 @@
-public class Gen{
+public class Gen {
     public static void main(String args[]){
         int [] arr = {5, 2, 8, 1, 9};
 
         for(int i = 0; i<arr.length - 1; i++){
-            for(int j = 0; i<arr.length-i-1; j++){
-                if(arr[j] > arr[j + 1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
+            int minIndex = i;
+
+            for(int j = i + 1; j<arr.length; j++){
+                if(arr[j] < arr[minIndex]);
+                minIndex = j;
             }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
         }
         for(int num : arr){
             System.out.println(num + " ");
