8dfbb99eb6d49dbca1fadefac4056b5f55e6298d
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-19 08:50:04 +0530

    Bubble Sort

--- Gen.java
+++ Gen.java
@@ -1,11 +1,18 @@
-import java.util.*;
+public class Gen{
+    public static void main(String args[]){
+        int [] arr = {5, 2, 8, 1, 9};
 
-public class Gen {
-    public static void main(String[] args) {
-        Integer[] arr = {5, 2, 8, 1, 9};
-
-        Arrays.sort(arr, Collections.reverseOrder());
-
-        System.out.println(Arrays.toString(arr));
+        for(int i = 0; i<arr.length - 1; i++){
+            for(int j = 0; i<arr.length-i-1; j++){
+                if(arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for(int num : arr){
+            System.out.println(num + " ");
+        }
     }
 }
