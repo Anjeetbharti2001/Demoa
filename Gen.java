10caa402bdec53296249eb9622af745a43a038cb
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-20 17:54:57 +0530

    Remove extra Space

--- Gen.java
+++ Gen.java
@@ -1,6 +1,6 @@
 public class Gen {
     public static void main(String args[]){
         String str = "Anjeet Bharti";
-        System.out.println(str.indexOf(" Bharti"));
+        System.out.println(str.trim());
     }
 }
