a99865ea5436e06011166970be15608cbcd089d7
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-20 17:51:27 +0530

    count of word using string

--- Gen.java
+++ Gen.java
@@ -1,7 +1,6 @@
 public class Gen {
     public static void main(String args[]){
         String str = "Anjeet Bharti";
-        System.out.println(str.equals("Anjeet Bharti"));
-        
+        System.out.println(str.indexOf(" Bharti"));
     }
 }
