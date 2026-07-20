d776b5a605d82b4fa5642d70c106b813fc933cbf
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-20 17:46:31 +0530

    Equal word true or false using string

--- Gen.java
+++ Gen.java
@@ -1,6 +1,7 @@
-public class Gen{
+public class Gen {
     public static void main(String args[]){
         String str = "Anjeet Bharti";
-        System.out.println(str.replace("Bharti", "Dear"));
+        System.out.println(str.equals("Anjeet Bharti"));
+        
     }
 }
