bdb195997b98261d2c5a95372d3719a7dd89eb91
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-20 17:23:46 +0530

    find the character of string

--- Gen.java
+++ Gen.java
@@ -1,6 +1,6 @@
-public class Gen{
+public class Gen {
     public static void main(String args[]){
         String str = "Anjeet Bharti";
-        System.out.println(str.toLowerCase());
+        System.out.println(str.charAt(7));
     }
 }
