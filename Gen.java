6c67da1f2d6a3ac10ae6c9e9d8ac033113a8458f
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-20 17:17:33 +0530

    To Print LoverCase

--- Gen.java
+++ Gen.java
@@ -1,6 +1,6 @@
 public class Gen{
     public static void main(String args[]){
         String str = "Anjeet Bharti";
-        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
     }
 }
