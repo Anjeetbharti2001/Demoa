317631d00471f4369fbf7da2a09828f8bca38f9c
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-20 17:11:36 +0530

    String to UpperCase

--- Gen.java
+++ Gen.java
@@ -1,6 +1,6 @@
-public class Gen {
+public class Gen{
     public static void main(String args[]){
         String str = "Anjeet Bharti";
-        System.out.println(str.length());
+        System.out.println(str.toUpperCase());
     }
 }
