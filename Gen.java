a8681c8872ea6795b63608843bf0d401f9d45f38
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-21 17:08:19 +0530

    check this word palindrome yes or no

--- Gen.java
+++ Gen.java
@@ -1,6 +1,20 @@
 public class Gen {
+    public static boolean isPalindrome(String str){
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right){
+            if(str.charAt(left) != str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
     public static void main(String args[]){
-        String str = "Anjeet Bharti";
-        System.out.println(str.trim());
+        String str = "madam";
+
+        System.out.println(isPalindrome(str));
     }
 }
