223a8027c7b26dbb853e8d1a79f8362e5654f9f2
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-21 17:37:57 +0530

    create a object from a class

--- Gen.java
+++ Gen.java
@@ -1,11 +1,9 @@
+// import java.util.*;
 public class Gen {
+    int x = 5;
     public static void main(String args[]){
-         String first = "Anjeet";
-         String second = "Bharti";
-
-         String result = first + " " + second;
-
-         System.out.println(result); // hello java
-
+        Gen myObj = new Gen();
+        
+        System.out.println(myObj.x);
     }
 }
