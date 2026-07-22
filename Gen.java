7ef59368a655c5eb5fb50ce96b81fbab5c2f45b6
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-22 08:19:43 +0530

    use of method create a object

--- Gen.java
+++ Gen.java
@@ -1,19 +1,9 @@
 public class Gen {
-    public static void main(String args[]) {
-        int i = 2;
-        switch (i) {
-            case 1:
-                System.out.println("I am in Case 1");
-                break;
-            case 2:
-                System.out.println("I am in case 2");
-                break;
-            case 3:
-                System.out.println("I am in case 3");
-                break;
-            default:
-                System.out.println("I am in default ");
-                break;
-        }
+    static void myMethod(){
+        System.out.println("Anjeet Bharti");
+    }
+    public static void main(String args[]){
+        myMethod();
+        
     }
 }
