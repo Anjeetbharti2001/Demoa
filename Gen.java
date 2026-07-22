96febf1a687f8914b2aba044f032771f9f52d970
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-22 08:37:00 +0530

    Constructer Parameter

--- Gen.java
+++ Gen.java
@@ -1,10 +1,10 @@
 public class Gen {
-    int x;
-    public Gen(){
-        x = 5;
+    int x ;
+    public Gen(int y){
+        x = y;
     }
     public static void main(String args[]){
-        Gen myObj = new Gen();
+        Gen myObj = new Gen(5);
         System.out.println(myObj.x);
 
     }
