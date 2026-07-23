5805382ab52019adc6aa473c4ae579e6f6e7b9ee
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-23 09:15:11 +0530

    using count character

--- Gen.java
+++ Gen.java
@@ -1,34 +1,36 @@
 import java.util.*;
-public class Gen{
-    public static boolean isAnagram(String str1, String str2){
-        // Remove spaces and convert to loverCase
+
+public class Gen {
+
+    public static boolean isAnagram(String str1, String str2) {
+
+        // Remove spaces and convert to lowercase
         str1 = str1.replaceAll("\\s", "").toLowerCase();
         str2 = str2.replaceAll("\\s", "").toLowerCase();
 
-        //if Length is different, not anagram
-        if(str1.length() != str2.length()){
+        // If length is different, not anagram
+        if (str1.length() != str2.length()) {
             return false;
         }
-        // convert string to character arrays 
-        char[] arr1 = str1.toCharArray();
-        char[] arr2 = str2.toCharArray();
 
-        // sort both arrays
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        
-        // compare arrays
-        return Arrays.equals(arr1, arr2);
-    }
-    public static void main(String args[]){
-        String str1 = "listen";
-        String str2 = "silent";
+        int[] count = new int[26];
 
-        if(isAnagram(str1, str2)){
-            System.out.println("String are Anagram");
-        }else{
-            System.out.println("String are not Anagram ");
+        for (int i = 0; i < str1.length(); i++) {
+            count[str1.charAt(i) - 'a']++;
+            count[str2.charAt(i) - 'a']--;
         }
 
+        for (int c : count) {
+            if (c != 0) {
+                return false;
+            }
+        }
+
+        return true; // Both strings are anagrams
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("listen", "silent"));
+        System.out.println(isAnagram("Hello", "world"));
     }
 }
