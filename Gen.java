07b5b1e6056bf09309ff4b2290eac4904f099f4b
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-24 03:20:21 +0530

    Defining and Using a class

--- Gen.java
+++ Gen.java
@@ -1,16 +1,32 @@
-class Student {
-    int id;
-    String n;
-
-    public Student(int id, String n){
-        this.id = id;
-        this.n = n;
-    }
-}
 public class Gen {
-    public static void main(String [] args){
-        Student s1 = new Student(10, "Alice");
-        System.out.println(s1.id);
-        System.out.println(s1.n);
+    String name;
+    String breed;
+    int age;
+    String color;
+    public Gen(String name, String breed, int age, String color){
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.color = color;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getBreed(){
+        return breed;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getColor(){
+        return color;
+    }
+    @Override
+    public String toString(){
+        return "Name is : " + name + "\nBreed age and color are :" + breed + " " + age + " " + color;
+    }
+    public static void main(String args[]){
+        Gen tuffy = new Gen("tuffy", "papillon", 5 , "white");
+        System.out.println(tuffy);
     }
 }
