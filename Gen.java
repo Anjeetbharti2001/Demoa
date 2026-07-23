3f81a28c92a6d140c4a98dfa8a5a1e8a9996e522
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-23 08:57:34 +0530

    check if two string are anagram

--- Gen.java
+++ Gen.java
@@ -1,12 +1,34 @@
+import java.util.*;
 public class Gen{
-   
+    public static boolean isAnagram(String str1, String str2){
+        // Remove spaces and convert to loverCase
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
 
+        //if Length is different, not anagram
+        if(str1.length() != str2.length()){
+            return false;
+        }
+        // convert string to character arrays 
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        // sort both arrays
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        
+        // compare arrays
+        return Arrays.equals(arr1, arr2);
+    }
     public static void main(String args[]){
-         int arr[] = {4,6,8,9};
-         System.out.println("Arrays Element ");
-         for(int num : arr){
-            System.out.print(num + " ");
-         }
+        String str1 = "listen";
+        String str2 = "silent";
+
+        if(isAnagram(str1, str2)){
+            System.out.println("String are Anagram");
+        }else{
+            System.out.println("String are not Anagram ");
+        }
 
     }
 }
