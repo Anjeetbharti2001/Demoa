2b9a04decb7514b867a9ff7ee1d02c10ac340bfe
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-25 09:25:16 +0530

    loop scops

--- Gen.java
+++ Gen.java
@@ -1,6 +1,7 @@
 public class Gen {
     public static void main(String args[]){
-        int x = 100;
-        System.out.println(x);
+        for(int i = 0; i< 5; i++){
+            System.out.println(i);
+        }
     }
 }
