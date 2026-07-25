ad8d1e4bcab5102e436c389aa814c823efcce02c
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-25 09:31:39 +0530

    use recursion to add all numbers from 1 to 10

--- Gen.java
+++ Gen.java
@@ -1,7 +1,13 @@
 public class Gen {
-    public static void main(String args[]){
-        for(int i = 0; i< 5; i++){
-            System.out.println(i);
+    public static int sum(int k){
+         if(k > 0){
+            return k + sum(k - 1);
+        }else{
+            return 0;
         }
+    }
+    public static void main(String args[]){
+       int result = sum(10);
+       System.out.println(result);
     }
 }
