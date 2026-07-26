47654a5519dd8997cb9f74fbd066da1a3d1b72a3
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-26 07:45:32 +0530

    Traversal An arrays using Enhanced for- each loops

--- Gen.java
+++ Gen.java
@@ -1,10 +1,10 @@
 public class Gen {
     public static void main(String args[]){
-        int [] arr = {40, 60, 20, 10};
-        System.out.println("Arrays element:-");
+        int arr[] = {3,5,6,7,8};
+        System.out.println("Arrays element :-");
 
-        for(int i = 0; i<arr.length; i++){
-            System.out.println(arr[i]);
+        for(int num : arr){
+            System.out.print(num + " ");
         }
     }
 }
