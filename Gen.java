b44bec2b6189e9f32fdbd9fa0ebb4bc3c725bf63
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-26 08:16:44 +0530

    binarySearch

--- Gen.java
+++ Gen.java
@@ -1,21 +1,30 @@
-import java.util.Scanner;
-
-public class Gen {
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter Size Of an Arrays:-");
-        int n = sc.nextInt();
-
-        int []arr = new int[n];
-        System.out.println("Enter arrays element :-");
-
-        for(int i = 0; i<n; i++){
-            arr[i] = sc.nextInt();
+public class Gen{
+    static int binarySearch(int arr[], int target){
+        int left = 0;
+        int right = arr.length - 1;
+        while(left <= right){
+            int mid = left + (right - left)/ 2;
+            if(arr[mid] == target){
+                return mid;
+            }else if(arr[mid] < target){
+                left = mid + 1;
+            }else if(arr[mid] < target){
+                left = mid + 1;
+            }else {
+                right =  mid - 1;
+            }
         }
+        return - 1;
+    }
+    public static void main(String args[]){
+        int arr[] = {4,8,9,3,9};
+        int target = 50;
 
-        for(int i = 0; i<n; i++){
-            System.out.print(arr[i] + " ");
+        int index = binarySearch(arr, target);
+        if(index != -1){
+            System.out.println("Element found at index : " + index);
+        }else {
+            System.out.println("Element not found ");
         }
     }
 }
