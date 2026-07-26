a73d35d30c352b24140f5f5b76a4860abc430d43
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-26 08:32:39 +0530

    Decending order in arrays sort

--- Gen.java
+++ Gen.java
@@ -1,8 +1,22 @@
+// import java.util.*;
+// import java.util.Collections;
+
+// public class Gen{
+//     public static void main(String args[]){
+//         int [] arr = {3, 8, 3, 1, 5};
+
+//         Arrays.sort(arr, Collections.reverseOrder());
+//         System.out.println(Arrays.toString(arr));
+//     }
+// }
+
 import java.util.*;
+
 public class Gen {
-    public static void main(String args[]){
-        int [] arr = {4, 3,5,6,1,9};
-        Arrays.sort(arr);
+    public static void main(String args[]) {
+        Integer[] arr = {3, 8, 3, 1, 5};
+
+        Arrays.sort(arr, Collections.reverseOrder());
 
         System.out.println(Arrays.toString(arr));
     }
