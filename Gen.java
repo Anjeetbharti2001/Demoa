3eff7cee3c276ed74d57aab228f3cb377a136e4f
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-27 02:36:54 +0530

    String building and USE

--- Gen.java
+++ Gen.java
@@ -1,23 +1,16 @@
-// import java.util.*;
-// import java.util.Collections;
-
-// public class Gen{
-//     public static void main(String args[]){
-//         int [] arr = {3, 8, 3, 1, 5};
-
-//         Arrays.sort(arr, Collections.reverseOrder());
-//         System.out.println(Arrays.toString(arr));
-//     }
-// }
-
-import java.util.*;
-
 public class Gen {
-    public static void main(String args[]) {
-        Integer[] arr = {3, 8, 3, 1, 5};
+    public static void main(String args[]){
+        StringBuilder sb = new StringBuilder("Anjeet");
 
-        Arrays.sort(arr, Collections.reverseOrder());
+        // add text
+        sb.append("java");
 
-        System.out.println(Arrays.toString(arr));
+        // Insert text
+        sb.insert(6, "life");
+
+        //Reverse string 
+        sb.reverse();
+
+        System.out.println(sb);
     }
 }
