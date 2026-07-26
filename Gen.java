8d450ee7726cd26b7033204e5c37913d01cb2a8a
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-26 07:51:39 +0530

    Traversal an Arrays in reverse order

--- Gen.java
+++ Gen.java
@@ -1,10 +1,10 @@
 public class Gen {
     public static void main(String args[]){
-        int arr[] = {3,5,6,7,8};
-        System.out.println("Arrays element :-");
+        int [] arr = {1,4,6,8,9};
+        System.out.println("Reverse Traversal:-");
 
-        for(int num : arr){
-            System.out.print(num + " ");
+        for(int i = arr.length - 1; i>= 0; i--){
+            System.out.println(arr[i]);
         }
     }
 }
