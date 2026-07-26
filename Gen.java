f05351f4768b4090206b24ba009469929c051060
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-26 08:22:18 +0530

    Sort an arrays in Accending order

--- Gen.java
+++ Gen.java
@@ -1,30 +1,9 @@
-public class Gen{
-    static int binarySearch(int arr[], int target){
-        int left = 0;
-        int right = arr.length - 1;
-        while(left <= right){
-            int mid = left + (right - left)/ 2;
-            if(arr[mid] == target){
-                return mid;
-            }else if(arr[mid] < target){
-                left = mid + 1;
-            }else if(arr[mid] < target){
-                left = mid + 1;
-            }else {
-                right =  mid - 1;
-            }
-        }
-        return - 1;
-    }
+import java.util.*;
+public class Gen {
     public static void main(String args[]){
-        int arr[] = {4,8,9,3,9};
-        int target = 50;
+        int [] arr = {4, 3,5,6,1,9};
+        Arrays.sort(arr);
 
-        int index = binarySearch(arr, target);
-        if(index != -1){
-            System.out.println("Element found at index : " + index);
-        }else {
-            System.out.println("Element not found ");
-        }
+        System.out.println(Arrays.toString(arr));
     }
 }
