1f2593a791a3973cc255fc25207a8f234e50363f
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-28 09:22:03 +0530

    Sort an Array in Decending Order

--- Gen.java
+++ Gen.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class Gen {
 
     public static void main(String args[]){
-        int [] arr = {5, 2, 8, 1, 9};
-        Arrays.sort(arr);
+        Integer [] arr = {5, 2, 8, 1, 9};
+        Arrays.sort(arr, Collections.reverseOrder());
         System.out.println(Arrays.toString(arr));
     }
 }
