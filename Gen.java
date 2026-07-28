e32db101df7a9a0d0fba5fa19ba955839e7a15dd
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-28 09:29:22 +0530

    bubble Sort in arrays

--- Gen.java
+++ Gen.java
@@ -1,10 +1,19 @@
-// Sort an Arrays In Accending Order
-import java.util.*;
+// Bubble Sort 
 public class Gen {
-
     public static void main(String args[]){
-        Integer [] arr = {5, 2, 8, 1, 9};
-        Arrays.sort(arr, Collections.reverseOrder());
-        System.out.println(Arrays.toString(arr));
+        int [] arr = {5, 2, 8, 1, 9};
+
+        for(int i = 0; i<arr.length - 1; i++){
+            for(int j = 0; i<arr.length -i -1; i++){
+                if(arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for(int num : arr){
+            System.out.println(num + " ");
+        }
     }
 }
