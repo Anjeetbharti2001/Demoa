5a2b5094458756be687688b6a35ebfdf1ce8ba66
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-29 08:42:50 +0530

    for loops using the iteration print value

--- Gen.java
+++ Gen.java
@@ -1,13 +1,7 @@
 public class Gen {
     public static void main(String args[]) {
-        int score = 75;
-
-        if (score >= 90) {
-            System.out.println("Excellent");
-        } else if (score >= 70) {
-            System.out.println("Good"); // This block will execute
-        } else {
-            System.out.println("Needs Improvement");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Iteration: " + i);
         }
 
     }
