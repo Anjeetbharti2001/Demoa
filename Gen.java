f6c703b95062e7bd10670d9525d0d9552de554ca
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-30 20:55:52 +0530

    Declare and Initialize n Array

--- Gen.java
+++ Gen.java
@@ -1,5 +1,10 @@
 public class Gen {
     public static void main(String args[]){
-        System.out.println("my name : Anjeet I learn java Programming .");
+        int [] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("Arrays element :");
+        for(int i = 0; i< numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
     }
 }
