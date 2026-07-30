1fc88bd003dab5bf9f0faf813b4bfa3379d0a9a5
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-30 21:05:38 +0530

    Find the Sum of Array Element

--- Gen.java
+++ Gen.java
@@ -1,10 +1,23 @@
+import java.util.*;
+
 public class Gen {
     public static void main(String args[]){
-        int [] numbers = {10, 20, 30, 40, 50};
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Arrays element :");
-        for(int i = 0; i< numbers.length; i++){
-            System.out.println(numbers[i]);
+        System.out.print("Enter the size of the arrays:");
+        int n = sc.nextInt();
+
+        int []arr = new int[n];
+
+        System.out.println("Enter " + n + "element:");
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
         }
+
+        System.out.println("Array element are :");
+        for(int i = 0; i< n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
     }
 }
