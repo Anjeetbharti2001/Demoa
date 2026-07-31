377e6dc5e5d7ffe2020074c2d1152382786bc56e
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-07-31 16:37:38 +0530

    permutation word

--- Gen.java
+++ Gen.java
@@ -1,14 +1,25 @@
 public class Gen {
-    public static void main(String args[]){
-        int[] arr = {1, 5 , 7 , 8};
 
-        int max = arr[0];
-
-        for(int i = 1; i < arr.length; i++){
-            if(arr[i] > max){
-                max = arr[i];
-            }
+    public static void permute(String str, String ans) {
+        // Base case
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
         }
-        System.out.println("Largest element = " + max);
+
+        // Recursive case
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            // Remove current character
+            String remaining = str.substring(0, i) + str.substring(i + 1);
+
+            // Recursive call
+            permute(remaining, ans + ch);
+        }
+    }
+
+    public static void main(String[] args) {
+        permute("ABC", "");
     }
 }
