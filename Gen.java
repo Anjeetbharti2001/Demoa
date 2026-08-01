1d1f1420f76dbf5e2878f3ffdee1251d5fd507cd
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-01 18:08:15 +0530

    Linked list

--- Gen.java
+++ Gen.java
@@ -3,23 +3,38 @@ class Node {
     Node next;
     Node(int data){
         this.data = data;
+        this.next = null;
     }
 }
 public class Gen {
-    public static void main(String args[]){
-        Node first = new Node(3);
-        Node second = new Node(2);
-        Node third = new Node(1);
-
-        first.next = second;
-        second.next = third;
-
-        Node temp = first;
-
+    // Display Linked List 
+    public static void display(Node head){
+        Node temp = head;
 
         while(temp != null){
             System.out.print(temp.data + " ");
             temp = temp.next;
         }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        //Create nodes
+        Node first = new Node(4);
+        Node second = new Node(3);
+        Node third = new Node(2);
+        Node forth = new Node(1);
+
+        // Connet Nodes
+        first.next = second;
+        second.next = third;
+        third.next = forth;
+
+        //Head of the linked list
+
+        Node head = first;
+
+        // Display the linked list
+        System.out.println("Linked list: ");
+        display(head);
     }
 }
