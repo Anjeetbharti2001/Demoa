9346644c6f8907503d90ff7cbae502fad3ac1142
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-03 14:34:18 +0530

    create linked list in java

--- Gen.java
+++ Gen.java
@@ -11,10 +11,21 @@ public class Gen {
         Node first = new Node(2);
         Node second = new Node(3);
         Node third = new Node (4);
+        Node forth = new Node (5);
+        Node fifth = new Node (6);
+        Node sixth = new Node (7);
+        Node seventh = new Node(8);
+        Node eighth = new Node(9);
+
 
         first.next = second;
         second.next = third;
-
+        third.next = forth;
+        forth.next = fifth;
+        fifth.next = sixth;
+        sixth.next = seventh;
+        seventh.next = eighth;
+        
         Node temp = first;
 
         while(temp != null){
