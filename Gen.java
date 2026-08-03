10084379a26a5974125d3bc0d364c40176e261b1
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-03 13:46:15 +0530

    Merge two list in java

--- Gen.java
+++ Gen.java
@@ -1,12 +1,19 @@
+import java.util.*;
 public class Gen {
+    public static void main(String args[]){
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(10);
+        list1.add(20);
+        list1.add(30);
 
-    public static void main(String[] args) {
+        List<Integer>list2 = new ArrayList<>();
+        list2.add(40);
+        list2.add(50);
+        list2.add(60);
 
-        StringBuilder sb = new StringBuilder("Anjeet");
+        // Merge list2 into list1
+        list1.addAll(list2);
 
-        sb.append(" Bharti");
-        sb.reverse();
-
-        System.out.println(sb);
+        System.out.println("Merge list: " + list1);
     }
 }
