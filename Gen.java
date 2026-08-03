94483624aaee9c44f004f9a8aeff56351249bca9
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-03 15:35:19 +0530

    Traversal linked List

--- Gen.java
+++ Gen.java
@@ -1,36 +1,34 @@
-class Node {
+class Node{
     int data;
     Node next;
-    Node(int data){
-        this.data= data;
-        this.next = null;
+    Node (int data){
+      this.data = data;
+      this.next = null;
     }
 }
 public class Gen {
-    public static void main(String args[]){
-        Node first = new Node(2);
-        Node second = new Node(3);
-        Node third = new Node (4);
-        Node forth = new Node (5);
-        Node fifth = new Node (6);
-        Node sixth = new Node (7);
-        Node seventh = new Node(8);
-        Node eighth = new Node(9);
-
-
-        first.next = second;
-        second.next = third;
-        third.next = forth;
-        forth.next = fifth;
-        fifth.next = sixth;
-        sixth.next = seventh;
-        seventh.next = eighth;
-        
-        Node temp = first;
+    // traversal Function 
+    public static void traverse(Node head){
+        Node temp = head;
 
         while(temp != null){
-            System.out.print (temp.data + " ");
+            System.out.print(temp.data + " ");
             temp = temp.next;
         }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        // Create a linked list 
+        Node head  = new Node(4);
+        head.next = new Node (5);
+        head.next.next = new Node(6);
+        head.next.next.next = new Node (7);
+        head.next.next.next.next = new Node (8);
+        head.next.next.next.next.next = new Node(9);
+
+        // Traversal Function
+        
+        System.out.println("Linked list :-");
+        traverse(head);
     }
 }
