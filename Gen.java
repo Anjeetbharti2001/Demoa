7252db5e28e655a5fa89a66ea5b542639b18c9cd
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-03 14:26:40 +0530

    Create a linked list in java

--- Gen.java
+++ Gen.java
@@ -1,15 +1,25 @@
-import java.util.*;
+class Node {
+    int data;
+    Node next;
+    Node(int data){
+        this.data= data;
+        this.next = null;
+    }
+}
 public class Gen {
     public static void main(String args[]){
-      
-        List<Integer> list1 = List.of(10, 20, 30);
-        List<Integer> list2 = List.of(40, 50, 60);
+        Node first = new Node(2);
+        Node second = new Node(3);
+        Node third = new Node (4);
 
-        List<Integer>mergedList = new ArrayList<>();
+        first.next = second;
+        second.next = third;
 
-        mergedList.addAll(list1);
-        mergedList.addAll(list2);
+        Node temp = first;
 
-        System.out.println("Merge List : " + mergedList);
+        while(temp != null){
+            System.out.print (temp.data + " ");
+            temp = temp.next;
+        }
     }
 }
