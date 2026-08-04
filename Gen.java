433d5426ac00b4de2526f07259d3d48ba2d04be6
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-04 10:50:08 +0530

    InserAt Beginning linked list

--- Gen.java
+++ Gen.java
@@ -1,30 +1,88 @@
-// store data
-class Node {
-    int data;
-    Node next;
+//  class Node{
+//     int data;
+//     Node next;
+//     Node(int data){
+//         this.data = data;
+//         this.next = null;
+//     }
+// }
 
-    Node(int data) {
-        this.data = data;
-        this.next = null;
-    }
-}
+// // Insert at beginning 
+// public static Node insertAtBeginning(Node head, int data){
+//     Node newNode = new Node(data);
+//     newNode.next = head;
+//     head = newNode;
+//     return head;
+// }
+// public class Gen {
+//     public static void display(Node head){
+//         Node temp = head;
+
+//         while(temp != null){
+//             System.out.print(temp.data + " ");
+//             temp = temp.next;
+//         }
+//         System.out.println("null");
+//     }
+//     public static void main(String args[]){
+//       Node head = new Node(5);
+//       head.next = new Node(2);
+//       head.next.next = new Node(8);
+
+//       System.out.println("Before insertion :");
+//       display(head);
+
+//       head = insertAtBeginning(head, 5);
+
+//       System.out.println("After insertion :");
+//       display(head);
+//     }
+// }
 
 public class Gen {
-    public static void traverse(Node head) {
-        Node temp = head;
 
-        while (temp != null) {
-            System.out.print(temp.data + " ");
-            temp = temp.next;
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
         }
     }
 
-    public static void main(String args[]) {
-        // create node
-        Node head = new Node(8);
-        head.next = new Node(6);
-        head.next.next = new Node(5);
+    // Insert at beginning
+    public static Node insertAtBeginning(Node head, int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+        return head;
+    }
 
-        traverse(head);
+    // Display Linked List
+    public static void display(Node head) {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;   // Fixed
+        }
+
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        Node head = new Node(5);
+        head.next = new Node(2);
+        head.next.next = new Node(8);
+
+        System.out.println("Before insertion:");
+        display(head);
+
+        head = insertAtBeginning(head, 10);
+
+        System.out.println("After insertion:");
+        display(head);
     }
 }
