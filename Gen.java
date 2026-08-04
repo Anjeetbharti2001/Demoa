b669ab8ba127de53a77f262835afd662a7e5c9c9
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-04 09:33:36 +0530

    new Create linked list

--- Gen.java
+++ Gen.java
@@ -1,30 +1,29 @@
 class Node {
     int data;
     Node next;
-    Node (int data){
+
+    Node(int data) {
         this.data = data;
         this.next = null;
     }
 }
+
 public class Gen {
-    public static void main(String args[]){
-        Node first = new Node(50);
-        Node second = new Node (90);
-        Node third = new Node (55);
-        Node fourth = new Node (77);
-        Node fifth = new Node (40);
+    public static void main(String args[]) {
+
+        // create a linked list
+        Node first = new Node(3);
+        Node second = new Node(8);
+        Node third = new Node(6);
 
         first.next = second;
         second.next = third;
-        third.next = fourth;
-        fourth.next = fifth;
-
 
         Node temp = first;
 
-        while(temp != null){
-             System.out.println(temp.data + " ");
-             temp = temp.next;
+        while (temp != null) {
+            System.out.println(temp.data + " ");
+            temp = temp.next;
         }
     }
 }
