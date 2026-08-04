7ab775bcfee6419f7ef4522ad9b20f296edc1094
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-04 09:56:09 +0530

    Traversal linked list

--- Gen.java
+++ Gen.java
@@ -9,21 +9,24 @@ class Node {
 }
 
 public class Gen {
-    public static void main(String args[]) {
 
-        // create a linked list
-        Node first = new Node(3);
-        Node second = new Node(8);
-        Node third = new Node(6);
-
-        first.next = second;
-        second.next = third;
-
-        Node temp = first;
+    public static void travese(Node head) {
+        Node temp = head;
 
         while (temp != null) {
-            System.out.println(temp.data + " ");
+            System.out.print(temp.data + " ");
             temp = temp.next;
         }
+    }
+
+    public static void main(String args[]) {
+        // Create a node
+        Node head = new Node(4);
+        head.next = new Node(5);
+        head.next.next = new Node(7);
+        head.next.next.next = new Node(3);
+
+        travese(head);
+
     }
 }
