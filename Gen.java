a805147e298867d008ed5314e295981c9e91d2ad
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-06 17:06:30 +0530

    None of the above

--- Gen.java
+++ Gen.java
@@ -1,9 +1,5 @@
-public class Gen{
+public class Gen {
     public static void main(String args[]){
-        message();
-        System.out.println("cry , and you stop the monotony!");
-    }
-    static void message(){
-       System.out.println("Smile , and the world smile with you..");
+        System.out.println(7 + "\n" + 5);
     }
 }
