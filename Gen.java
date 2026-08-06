c97e58f65d5d92732509554e8cc4656157f7634e
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-06 17:41:39 +0530

    write a program to output the squares the(using multiplication) of numbers from 1 to 5 on separate line

--- Gen.java
+++ Gen.java
@@ -1,5 +1,9 @@
 public class Gen {
-    public static void main(String args[]){
-        System.out.println(1 + " " + 2 + " " + 3 + " " + 4 + " " + 5);
+    public static void main(String args[]) {
+        System.out.println(1 + " - " + (1 * 1));
+        System.out.println(2 + " - " + (2*2));
+        System.out.println(3 + " - " + (3*3));
+        System.out.println(4 + " - " + (4*4));
+        System.out.println(5 + " - " + (5*5));
     }
 }
