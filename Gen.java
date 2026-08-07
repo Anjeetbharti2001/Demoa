8fade6478d2f3b13ebf975f21eab6c6dd4438c8f
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-07 17:42:30 +0530

    Convert Temperature

--- Gen.java
+++ Gen.java
@@ -1,9 +1,7 @@
 public class Gen {
   public static void main(String args[]){
-    int x = 20;
-    int y = 6;
-    int div = x / y;
-
-    System.out.println(div);
+    double temperature = 25.5;
+    System.out.println("Celsius -" + temperature);
+     System.out.println("Kelvin - " + (temperature + 273));
   }
 }
