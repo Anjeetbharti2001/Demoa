cf16ec00c4878759da121dac8e5f9a12ef404a73
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-07 17:20:57 +0530

    Declare Long data type

--- Gen.java
+++ Gen.java
@@ -1,6 +1,6 @@
-public class Gen{
+public class Gen {
   public static void main(String args[]){
-    char ch = 'B';
-    System.out.println(ch);
+    long num = 16734524339L;
+    System.out.println(num);
   }
 }
