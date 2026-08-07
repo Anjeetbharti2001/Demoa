abdcb6049975f97592429bed384d7bf8fadab3fc
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-07 08:53:01 +0530

    Declaration variable

--- Gen.java
+++ Gen.java
@@ -1,9 +1,6 @@
-public class Gen {
-    public static void main(String args[]) {
-        System.out.println(1 + " - " + (1 * 1));
-        System.out.println(2 + " - " + (2*2));
-        System.out.println(3 + " - " + (3*3));
-        System.out.println(4 + " - " + (4*4));
-        System.out.println(5 + " - " + (5*5));
+public class Gen{
+    public static void main(String args[]){
+        int age = 25;
+        System.out.println(age);
     }
 }
