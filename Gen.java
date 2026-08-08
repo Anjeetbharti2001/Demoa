7fd616fafe78a97179c06c4c1c3693d84f688a28
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-08 17:15:25 +0530

    using Enharenced Traversal an Arrays

--- Gen.java
+++ Gen.java
@@ -1,10 +1,10 @@
-public class Gen{
+public class Gen {
   public static void main(String args[]){
-    int [] arr = {10, 20, 30, 40, 50, 60};
-    System.out.println("Element Arrayus:-" );
+    int arr[] = {4, 7, 9, 5, 2};
+    System.out.println("Arrays element:-");
 
-    for(int i = 0; i<arr.length; i++){
-      System.out.println(arr[i]);
+    for(int num : arr){
+      System.out.println(num);
     }
   }
 }
