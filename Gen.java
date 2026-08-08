f02ef74525e9f8d8ecdb732163d66eb32fb9b8b8
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-09 03:40:37 +0530

    Implementation of java name

--- Gen.java
+++ Gen.java
@@ -1,10 +1,5 @@
-public class Gen {
+public class Gen{
   public static void main(String args[]){
-    int arr[] = {4, 7, 9, 5, 2};
-    System.out.println("Arrays element:-");
-
-    for(int num : arr){
-      System.out.println(num);
-    }
+    System.out.println("Anjeet Bharti");
   }
 }
