c59aaac42434a94c15085807480aa904168b65b7
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-08 16:39:47 +0530

    New Create a linkedList

--- Gen.java
+++ Gen.java
@@ -1,7 +1,27 @@
+class Node {
+  int data;
+  Node next;
+
+  Node(int data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
 public class Gen {
-  public static void main(String args[]){
-    double temperature = 25.5;
-    System.out.println("Celsius -" + temperature);
-     System.out.println("Kelvin - " + (temperature + 273));
+  public static void main(String args[]) {
+    Node First = new Node(10);
+    Node second = new Node(29);
+    Node third = new Node(19);
+
+    First.next = second;
+    second.next = third;
+
+    Node temp = First;
+
+    while (temp != null) {
+      System.out.println(temp.data + " ");
+      temp = temp.next;
+    }
   }
 }
