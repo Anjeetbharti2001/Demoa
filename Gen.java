a4da396ac2aef5e9f0bbd58c0fc4654ee4eb812a
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-08 17:10:35 +0530

    Traversing an Arrays using for loops

--- Gen.java
+++ Gen.java
@@ -1,21 +1,10 @@
-// moves all zero to end
-public class Gen {
-  public static void moveZeros(int arr[]){
-    int j = 0;
-    for(int i = 0; i<arr.length; i++){
-      if(arr[i] != 0){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-        j++;
-      }
-    }
-    for(int x : arr){
-      System.out.println(x + " ");
-    }
-  }
+public class Gen{
   public static void main(String args[]){
-    int arr[] = {0, 1, 0, 3, 1, 2};
-    moveZeros(arr);
+    int [] arr = {10, 20, 30, 40, 50, 60};
+    System.out.println("Element Arrayus:-" );
+
+    for(int i = 0; i<arr.length; i++){
+      System.out.println(arr[i]);
+    }
   }
 }
