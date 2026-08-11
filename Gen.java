216f17e2c291a6ff7dc540e8ec3b3b6054563a51
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-11 11:47:52 +0530

    Bubble Sort arrays

--- Gen.java
+++ Gen.java
@@ -1,30 +1,18 @@
-import java.util.*;
-
-public class Gen{
+public class Gen {
   public static void main(String args[]){
-    Stack<Integer> Stack = new Stack<>();
-    //push 
-    Stack.push(20);
-    Stack.push(20);
-    Stack.push(28);
-    Stack.isEmpty();
-    
-    System.out.println(Stack);
+    int []arr = {5, 2, 8, 1, 9};
 
-    // peek
-    // System.out.println("Top Element :-" + Stack.peek());
-
-    // // pop
-    // System.out.println("Remove :- " + Stack.pop());
-    // System.out.println(Stack);
-
-    // Size
-    // System.out.println("Size : " + Stack.size());
-    // System.out.println(Stack);
-
-    // Empty
-
-    System.out.println("IsEmpty :-" + Stack.isEmpty());
-    System.out.println(Stack);
+    for(int i = 0; i<arr.length - 1; i++){
+      for(int j = 0; j<arr.length - i - 1; j++){
+        if(arr[j] > arr[j + 1]){
+          int temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+    for(int num : arr){
+      System.out.println(num + " ");
+    }
   }
 }
