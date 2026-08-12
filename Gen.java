3dfc45355e1243152fd4f244094bf3abae64f2fb
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-12 10:21:48 +0530

    Min Heap priority Queue

--- Gen.java
+++ Gen.java
@@ -1,19 +1,16 @@
 import java.util.*;
-public class Gen {
+
+public class Gen{
   public static void main(String args[]){
-    int arr[] = {3, 1, 5, 12, 2, 11};
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-    int k = 3;
+    pq.add(30);
+    pq.add(10);
+    pq.add(20);
 
-    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
-    for(int num : arr){
-      minHeap.add(num);
-
-      if(minHeap.size() > k){
-        minHeap.poll();
-      }
-    }
-    System.out.println("Top " + k + "Elements " + minHeap);
+    System.out.println(pq.poll());
+    System.out.println(pq.poll());
+    System.out.println(pq.poll());
   }
 }
