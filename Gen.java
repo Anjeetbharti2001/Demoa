149044ab1e4efa2a161c8dd0d725941bb623fe78
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-13 02:19:08 +0530

    check palindrome yes or no

--- Gen.java
+++ Gen.java
@@ -1,19 +1,13 @@
 public class Gen{
-    public static boolean isPalindrome(String str){
-        int left = 0;
-        int right = str.length() - 1;
-
-        while(left < right){
-            if(str.charAt(left) != str.charAt(right)){
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
-    }
-    public static void main(String args[]){
+       public static void main(String args[]){
         String str = "madam";
-        System.out.println(isPalindrome(str));
+        String rev = new StringBuilder(str).reverse().toString();
+     
+        
+        if(str.equals(rev)){
+            System.out.println("palindrome");
+        }else {
+            System.out.println("Not Palindrom:-");
+        }
     }
 }
