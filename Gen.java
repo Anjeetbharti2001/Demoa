507d0994f10255d7f52268bca14aa4fbdbe1b808
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-13 02:04:15 +0530

    String palindrome

--- Gen.java
+++ Gen.java
@@ -1,24 +1,19 @@
-class TreeNode{
-  int data;
-  TreeNode left;
-  TreeNode right;
+public class Gen{
+    public static boolean isPalindrome(String str){
+        int left = 0;
+        int right = str.length() - 1;
 
-  TreeNode(int data){
-    this.data = data;
-    left = null;
-    right = null;
-  }
-}
-public class Gen {
-  public static void main(String args[]){
-    TreeNode root = new TreeNode(10);
-
-    root.left = new TreeNode(5);
-    root.right = new TreeNode(15);
-
-    root.left.left = new TreeNode(3);
-    root.right = new TreeNode(7);
-
-    System.out.println(root.data);
-  }
+        while(left < right){
+            if(str.charAt(left) != str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static void main(String args[]){
+        String str = "madam";
+        System.out.println(isPalindrome(str));
+    }
 }
