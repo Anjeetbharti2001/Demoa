75900541a8266d710d3180d58a490448fe5628d6
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-15 03:19:42 +0530

    Sort an Arrays in Accending Order

--- Gen.java
+++ Gen.java
@@ -1,9 +1,9 @@
 import java.util.*;
 public class Gen {
     public static void main(String args[]){
-        Integer[] arr = {5, 2, 8, 1, 9};
+        int[] arr = {5, 2, 8, 1, 9};
 
-        Arrays.sort(arr, Collections.reverseOrder());
+        Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
