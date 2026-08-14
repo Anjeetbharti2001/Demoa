10caf3ac58f47cb621613108e776680b0c0c525a
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-15 03:16:54 +0530

    Sort an Arrays in Descending Order

--- Gen.java
+++ Gen.java
@@ -1,16 +1,9 @@
 import java.util.*;
-
 public class Gen {
-    public static void main(String args[]) {
+    public static void main(String args[]){
+        Integer[] arr = {5, 2, 8, 1, 9};
 
-        String str = "BANANA";
-
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        for (char ch : str.toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
-
-        System.out.println(map);
+        Arrays.sort(arr, Collections.reverseOrder());
+        System.out.println(Arrays.toString(arr));
     }
 }
