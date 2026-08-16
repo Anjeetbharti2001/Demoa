7d85a7a811a72bcd9567442212406ba866cd84a5
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-16 19:36:46 +0530

    java I n Add two number

--- Gen.java
+++ Gen.java
@@ -1,9 +1,5 @@
-import java.util.*;
 public class Gen {
     public static void main(String args[]){
-        int[] arr = {5, 2, 8, 1, 9};
-
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        System.out.println("Hello World");
     }
 }
