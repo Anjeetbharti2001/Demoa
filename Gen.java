fc577ffc1f98bb6ef339704b24491d35d9dadd57
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-16 19:42:35 +0530

    add two number in java

--- Gen.java
+++ Gen.java
@@ -1,5 +1,9 @@
 public class Gen {
     public static void main(String args[]){
-        System.out.println("Hello World");
+        int a = 5;
+        int b = 8;
+
+        int sum = a + b;
+        System.out.println("Total sum :-" + sum);
     }
 }
