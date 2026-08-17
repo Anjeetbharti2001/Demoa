de4e566d9ddd4c241eb6b98e933221a0ee6a1fa3
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-17 21:17:24 +0530

    Job sequence algorithms

--- Gen.java
+++ Gen.java
@@ -1,15 +1,43 @@
+import java.util.*;
+class Job{
+    char id ;
+    int deadline, profit;
+
+    Job(char id, int deadline, int profit){
+        this.id = id;
+        this.deadline = deadline;
+        this.profit = profit;
+    }
+}
+
 public class Gen {
     public static void main(String args[]){
-        int m = 9, n = 5;
+        Job[] jobs = {
+            new Job('A', 2, 100),
+            new Job('b', 1, 19),
+            new Job('c', 2, 27),
+            new Job('d', 1,25),
+            new Job('E', 3, 15 ),
+        };
+        Arrays.sort(jobs, (a, b) -> b.profit - a.profit);
 
-        System.out.println("Before swapping: m = " + m
-                           + ", n = " + n);
+        int maxDeadline = 3;
+        char[] result = new char [maxDeadline];
+        boolean[] slot = new boolean[maxDeadline];
 
-                           n = m ^ n;
+        for(Job job : jobs){
+            for(int j = Math.min(maxDeadline, job.deadline) - 1; j>= 0; j--){
+                if(!slot[j]){
+                    slot[j] = true;
+                    result[j] = job.id;
+                    break;
+                }
+            }
+        }
+        System.out.println("Job sequence: ");
 
-                           m = m^n;
-
-                           System.out.println("After swapping: m = " + m
-                           + ", n = " + n);
+        for(char c : result){
+            System.out.print(c + " ");
+        }
     }
 }
