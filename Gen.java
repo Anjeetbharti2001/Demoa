f49dd331eddc5413bdc6571fdfe5ad30ed18507f
Anjeetbharti@2001 <anjeetbharti2.0@gmail.com>
2026-08-17 21:42:43 +0530

    convert The number Decimal to binary

--- Gen.java
+++ Gen.java
@@ -1,24 +1,24 @@
-
 public class Gen {
+    public void decToBinary(int n){
+        for(int i = 31; i >= 0; i--){
+            int k = n >> i;
 
-    static void decToBinary(int n){
-        int[] binaryNum = new int[1000];
-
-        int i = 0;
-
-        while(n > 0){
-            binaryNum[i] = n % 2;
-            n = n/2;
-            i++;
-        }
-        for(int j = i - 1; j >= 0; j--){
-            System.out.print(binaryNum[j]);
+            if((k & 1) > 0){
+                System.out.print("1");
+            }else{
+                System.out.print("0");
+            }
         }
     }
+    class geek{
+        
+    }
     public static void main(String args[]){
-        int n = 17;
+        Gen g = new Gen();
+        int n = 32;
         System.out.println("Decimal - " + n);
         System.out.print("Binary - ");
-        decToBinary(n);
+
+        g.decToBinary(n);
     }
 }
